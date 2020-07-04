bb12b24360e5c7a96511bb8e6f18cdb5877f05af
Leonieke Hartmanns <leonieke.hartmanns@ziggo.nl>
2020-07-04 18:32:27 +0200

    added html-webpack-plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,9 @@
 const path = require("path")
 const webpack = require("webpack")
+const HtmlWebpackPlugin = require("html-webpack-plugin")
 module.exports = {
-	entry: './src/client/index.js'
-}
-module: {
+	entry: './src/client/index.js',
+    module: {
             rules: 
 			[
               {
@@ -21,4 +21,13 @@ module: {
 		        },
               },
             ]
-    }
+    },
+	plugins: [
+		new HtmlWebpackPlugin({
+			template: "./src/client/views/index.html",
+			filename: "./index.html",
+		})
+	
+	]
+	
+}
