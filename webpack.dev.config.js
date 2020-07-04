ea242562529ad344857a3ff3ffb88821663de60a
Leonieke Hartmanns <leonieke.hartmanns@ziggo.nl>
2020-07-04 18:49:48 +0200

    Added mode production and development

---
+++ webpack.dev.config.js
@@ -0,0 +1,35 @@
+const path = require("path")
+const webpack = require("webpack")
+const HtmlWebpackPlugin = require("html-webpack-plugin")
+module.exports = {
+	mode: 'development',
+	devtool: 'source-map',
+	entry: './src/client/index.js',
+    module: {
+            rules: 
+			[
+              {
+                test: '/\.js$/',
+                exclude: /node_modules/,
+                loader: "babel-loader"
+              },
+			  {
+				enforce: 'pre',
+				test: /\.js$/,
+				exclude: /node_modules/,
+				loader: 'eslint-loader',
+				options: {
+					// eslint options (if necessary)
+		        },
+              },
+            ]
+    },
+	plugins: [
+		new HtmlWebpackPlugin({
+			template: "./src/client/views/index.html",
+			filename: "./index.html",
+		})
+	
+	]
+	
+}
