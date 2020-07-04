ae119a57b5bf315610176ed00a4a3eed3bbb71cf
Leonieke Hartmanns <leonieke.hartmanns@ziggo.nl>
2020-07-04 16:09:57 +0200

    Added babel and eslint

--- webpack.config.js
+++ webpack.config.js
@@ -3,3 +3,22 @@ const webpack = require("webpack")
 module.exports = {
 	entry: './src/client/index.js'
 }
+module: {
+            rules: 
+			[
+              {
+                test: '/\.js$/',
+                exclude: /node_modules/,
+                loader: "babel-loader"
+              },
+			  {
+				enforce: 'pre',
+				test: /\.js$/,
+				exclude: /node_modules/,
+				loader: 'eslint-loader',
+				options: {
+					// eslint options (if necessary)
+		        },
+              },
+            ]
+    }
